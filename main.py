ecd83fbb212bbcc8f29219cc2e62726d0adab489
ShapochkinaAlisa <94118995+ShapochkinaAlisa@users.noreply.github.com>
2022-11-26 01:55:17 +0400

    do res

--- main.py
+++ main.py
@@ -15,12 +15,31 @@ def start():
 def test():
     return render_template("another.html")
 
-
 @app.route('/ques', methods=["POST", "GET"])
 def base():
-    return render_template("base.html")
+    import sqlite3
+    con = sqlite3.connect("db_achievements.db")
+    cur = con.cursor()
+    user_id = 0
+    ans = []
+    f = []
+    for i in range(8):
+        prog = cur.execute(f"""SELECT prog{i} FROM users WHERE id = {user_id}""").fetchone()[0]
+        max_value = cur.execute(f"""SELECT max_value FROM quests_params WHERE id = {i}""").fetchone()[0]
+        ans.append([prog, max_value])
+    for j in range(4):
+        block_prog = cur.execute(f"""SELECT block_prog{j} FROM users WHERE id = {user_id}""").fetchone()[0]
+        f.append(block_prog)
+
+    return render_template("base.html", ans=ans, f=f)
+
 
 
 if __name__ == "__main__":
     app.register_blueprint(apiserver.blueprint)
     app.run(port=8033, host="127.0.0.1")
+
+
+
+
+
