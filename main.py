edef5bdd8b7ac6cd9d5ad0e300082b04ca6697ca
egor <shv>
2022-11-26 00:28:02 +0400

    Запросы

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, request
-from back.convert import convert_value
 import eel
 
 app = Flask(__name__)
@@ -11,18 +10,25 @@ def start():
     return render_template("start.html")
 
 
+@app.route('/test')
+def test():
+    return render_template("another.html")
+
+
 @app.route('/ques', methods=["POST", "GET"])
 def base():
     return render_template("base.html")
 
 
 @eel.expose
-def convert_value_py(value: float, from_cur: str, to_cur: str) -> float:
-    return 1
+def convert_value_py(msg: str) -> str:
+    return '1'
 
 
 if __name__ == "__main__":
     app.run(port=8033, host="127.0.0.1")
+    # eel.init('C:/Users/Shv_e/PycharmProjects/WINK_assistant')
+    # eel.start('templates/test.html', mode="default", size=(760, 760))
 
 '''if __name__ == "__main__":
     db_session.global_init("db/service.db")
